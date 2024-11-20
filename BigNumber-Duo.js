4341959e522a55f872ab6164342545583d8d059e
David Van Hoorebeke <david@luno.be>
2024-11-20 21:00:08 +0100

    tidy up and make numbers clickable

--- BigNumber-Duo.js
+++ BigNumber-Duo.js
@@ -1,53 +1,20 @@
 class BigNumberDuo extends HTMLElement {
-  
-  _fire(type, detail, options) {
-    const node = this.content;
-    options = options || {};
-    detail = (detail === null || detail === undefined) ? {} : detail;
-    const event = new Event(type, {
-      bubbles: options.bubbles === undefined ? true : options.bubbles,
-      cancelable: Boolean(options.cancelable),
-      composed: options.composed === undefined ? true : options.composed
-    });
-    event.detail = detail;
-    node.dispatchEvent(event);
-    return event;
-  }
-  
-  set hass(hass) {
-      // Initialize the content if it's not there yet.
-      if (!this.content) {
 
-        const card = document.createElement('HA-card');
-        this.content = document.createElement('div');
-        //this.content.style = 'card-content';
-        this.style.textAlign = 'center';
-        this.content.style.display = 'inline-block';
-        card.appendChild(this.content);
-        card.addEventListener('click', event => {
-          this._fire('hass-more-info', { entityId: this.config.entity });
-        });
-        this.appendChild(card);
-        
-        var content = this.content;
-        /*this.innerHTML = `
-          <ha-card>
-            <div class="card-content"></div>
-          </ha-card>
-        `;*/
-        //this.content = this.querySelector('div');
-      }
-  
-      const entityId = this.config.entity;
-      const entityId2 = this.config.entity2;
-      const title = this.config.title;
-      const icon = this.config.icon;
-      const icon2 = this.config.icon2;
-      const iconSource = this.config.iconSource;
-      const iconSource2 = this.config.iconSource2;
-      const color = this.config.color;
-      const color2 = this.config.color2;
-      const canGoBelowZero = this.config.CanGoBelowZero;
+  set hass(hass) {
+    this._hass = hass;
+    // Update the card in case anything has changed
+    if(!this._config) return; // Can't assume setConfig is called before hass is set
+
+      const entityId = this._config.entity;
+      const entityId2 = this._config.entity2;
+      const title = this._config.title;
+      const icon = this._config.icon;
+      const icon2 = this._config.icon2;
+      const iconSource = this._config.iconSource;
+      const iconSource2 = this._config.iconSource2;
+      const color = this._config.color;
+      const color2 = this._config.color2;
+      const canGoBelowZero = this._config.CanGoBelowZero;
       const entityState = Math.round(+hass.states[entityId].state * 10) / 10;
       const entityState2 = Math.round(+hass.states[entityId2].state * 10) / 10;
 
@@ -101,14 +68,50 @@ class BigNumberDuo extends HTMLElement {
 
       const unitOfMeasurement = hass.states[entityId].attributes["unit_of_measurement"];
       const unitOfMeasurement2 = hass.states[entityId2].attributes["unit_of_measurement"];
-  
-      this.content.innerHTML = `
-      <br/>
-      <div id="title">${title}</div>
-      <br/>
-      <div id="value">${iconText}${valueColor}&nbsp;<small>${unitOfMeasurement}</small></div>
-      <div id="value">${iconText2}${valueColor2}&nbsp;<small>${unitOfMeasurement2}</small></div>
-      <style>
+
+      this.divTitle.innerHTML = title;
+      this.div1.innerHTML = `${iconText}${valueColor}&nbsp;<small>${unitOfMeasurement}</small>`;
+      this.div1.addEventListener("click", () => 
+      {
+        const actionConfig = {
+          entity: entityId,
+          tap_action: {
+            action: "more-info",
+          }
+        };
+
+        const event = new Event("hass-action", {
+          bubbles: true,
+          composed: true,
+        });
+        event.detail = {
+          config: actionConfig,
+          action: "tap",
+        };
+        this.div1.dispatchEvent(event);
+      });
+
+      this.div2.innerHTML = `${iconText2}${valueColor2}&nbsp;<small>${unitOfMeasurement2}</small>`;
+      this.div2.addEventListener("click", () => 
+        {
+          const actionConfig = {
+            entity: entityId2,
+            tap_action: {
+              action: "more-info",
+            }
+          };
+          const event = new Event("hass-action", {
+            bubbles: true,
+            composed: true,
+          });
+          event.detail = {
+            config: actionConfig,
+            action: "tap",
+          };
+          this.div2.dispatchEvent(event);
+        });
+
+      this.styleSheet.innerHTML = `
         #value
         {
           font-size: 65px;
@@ -127,16 +130,15 @@ class BigNumberDuo extends HTMLElement {
           font-size: 28px;
           text-align: center;
           line-height: 28px;
-        }
-      </style>
-      `;
-
-      
+        }`;
     }
   
     // The user supplied configuration. Throw an exception and Home Assistant
     // will render an error card.
-    setConfig(config) {
+    setConfig(config) 
+    {
+      this._config = config;
+
       if (!config.entity) {
         throw new Error('You need to define an entity');
       }
@@ -146,7 +148,45 @@ class BigNumberDuo extends HTMLElement {
       if (!config.title) {
         throw new Error('You need to define an title');
       }
-      this.config = config;
+      
+
+      // Make sure this only runs once
+      if(!this.setupComplete) {
+        // A ha-card element should be the base of all cards
+        // Best practice, and makes themes and stuff work
+        const card = document.createElement("ha-card");
+        
+        // At this point, we don't necessarily know anything about the current state
+        // of anything, but we can set up the general structure of the card.
+        this.nameDiv = document.createElement("div");
+        card.appendChild(this.nameDiv);
+        
+        this.linebreak = document.createElement('br');
+        card.appendChild(this.linebreak);
+        
+        this.divTitle = document.createElement('div');
+        this.divTitle.id = "title";
+        //divTitle.content = title;
+        card.appendChild(this.divTitle);
+
+        this.linebreak2 = document.createElement('br');
+        card.appendChild(this.linebreak2);
+
+        this.div1 = document.createElement('div');
+        this.div1.id = "value";
+        card.appendChild(this.div1);
+        
+        this.div2 = document.createElement('div');
+        this.div2.id = "value";
+        card.appendChild(this.div2);
+        
+        this.styleSheet = document.createElement('style');
+        card.appendChild(this.styleSheet);
+
+        this.appendChild(card);
+        
+        this.setupComplete = true;
+      }
     }
   
     // The height of your card. Home Assistant uses this to automatically
